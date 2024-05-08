44c048d3f2f21dd565689223c4be4415e571c002
lucas <lucaslxc81@gmail.com>
2024-05-08 03:14:51 -0300

    agregue un select en el formulario de contacto

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ header.innerHTML = `
     <img src="../Recursos/Img/logo.png" alt="Imagen de logo" width="100px">
 </div>
 <h1>RESERVA NATURAL "LOS ROBLES"</h1>`;
+
+
+
+
+const form = document.querySelector('form');
