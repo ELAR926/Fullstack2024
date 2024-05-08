d8643e5f0f427900ae25693e174e7782fe621079
lucas <lucaslxc81@gmail.com>
2024-05-08 01:37:58 -0300

    Pequeño cambio en contacto

---
+++ script.js
@@ -0,0 +1,6 @@
+const header = document.querySelector('header');
+header.innerHTML = `
+<div id="Logo">
+    <img src="../Recursos/Img/logo.png" alt="Imagen de logo" width="100px">
+</div>
+<h1>RESERVA NATURAL "LOS ROBLES"</h1>`;
