929ca19f83448ecb7194e8c342c67055b916d3b7
lucas <lucaslxc81@gmail.com>
2024-07-08 12:00:39 -0300

    borre cosas duplicadas

--- src/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask import render_template
-from flaskext.mysql import MySQL
-
-app = Flask(__name__)
-MySQL = MySQL()
-
-if __name__ == '__main__':
-app.run(debug=True)
